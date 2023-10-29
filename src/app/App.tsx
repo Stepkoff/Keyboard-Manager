67c762af073cf7909dfcd292685857076b5d7d8f
Stepan Rudiak <Stepan.rudiak95@gmail.com>
2023-10-29 17:15:05 +0100

    modal with router test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,69 @@
 import './styles/Global.sass'
-import {useState } from "react";
 import {Modal} from "@/shared/ui/Modal";
+import {useSearchParams} from "react-router-dom";
+
+
 
 export const App = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const [openModal2, setOpenModal2] = useState(false);
+  const [params, setSearchParams] = useSearchParams();
+
+  const firstModalIsOpened = params.get('modal1') === 'opened' ?? false
+  const secondModalIsOpened = params.get('modal2') === 'opened' ?? false
+
+  console.log('modal1', firstModalIsOpened)
+  console.log('modal2', secondModalIsOpened)
+
+  const openModal1 = () => {
+    setSearchParams((prevParams) => {
+      prevParams.set('modal1', 'opened')
+      return prevParams
+    })
+  }
+  const openModal2 = () => {
+    setSearchParams((prevParams) => {
+      prevParams.set('modal2', 'opened')
+      return prevParams
+    })
+  }
+
+  const closeModal1 = () => {
+    setSearchParams((prevParams) => {
+      prevParams.set('modal1', 'closed')
+      return prevParams
+    })
+  }
+
+  const closeModal2 = () => {
+    setSearchParams((prevParams) => {
+      prevParams.set('modal2', 'closed')
+      return prevParams
+    })
+  }
+
+
+  // const [openModal, setOpenModal] = useState(false);
+  // const [openModal2, setOpenModal2] = useState(false);
 
   return (
     <div className={'App'}>
 
-      <button onClick={() => setOpenModal(true)}>open modal</button>
-      <Modal isOpened={openModal} onClose={() => setOpenModal(false)}>
-        <h2>Modal 1</h2>
-        <h3>(Press 'Esc' to close this modal)</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, enim.</p>
+      <button onClick={openModal1}>open modal</button>
+      <Modal isOpened={firstModalIsOpened} onClose={closeModal1}>
+        <h2 style={{marginBottom: '10px'}}>Modal 1</h2>
+        <p style={{marginBottom: '10px'}}>(Press 'Esc' to close this modal)</p>
+        <h4 style={{marginBottom: '10px'}}>
+          Reload the page to make sure the modal state stays the same!
+          Also when Esc pressed modals closed in the right order, even after page reload
+        </h4>
         <div>
-          <button onClick={() => setOpenModal2(true)}>Open modal 2</button>
-          <Modal isOpened={openModal2} onClose={() => setOpenModal2(false)}>
-            <h2>Modal 2</h2>
-            <h3>(Press 'Esc' to close this modal)</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dicta facere hic minus molestiae molestias nihil nobis, quae quas quis sequi ut voluptate? Asperiores assumenda ipsa modi nisi nostrum numquam.</p>
+          <button onClick={openModal2}>Open modal 2</button>
+          <Modal isOpened={secondModalIsOpened} onClose={closeModal2}>
+            <h2 style={{marginBottom: '10px'}}>Modal 2</h2>
+            <p style={{marginBottom: '10px'}}>(Press 'Esc' to close this modal)</p>
+            <h4 style={{marginBottom: '10px'}}>
+              Reload the page to make sure the modal state stays the same!
+              Also when Esc pressed modals closed in the right order, even after page reload
+            </h4>
           </Modal>
         </div>
       </Modal>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
 import ReactDOM from 'react-dom/client'
 import {App} from "@/app/App.tsx";
 import {StrictMode} from "react";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        index: true,
+        element: <App/>
+      }
+    ]
+  }
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 
-  // <App />
 
 )
